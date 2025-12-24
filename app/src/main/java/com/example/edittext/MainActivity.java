735ea29dab93135605b361888bcff5d1cdb1fd32
cwj <282330332@qq.com>
2025-12-24 20:00:45 +0800

    开发环境升级

--- app/src/main/java/com/example/edittext/MainActivity.java
+++ app/src/main/java/com/example/edittext/MainActivity.java
@@ -15,13 +15,26 @@ public class MainActivity extends AppCompatActivity {
     }
 
     /**
-     * 显示状态
+     * 表情测试
      * @param
      * @return
      */
-    public void onEmo(View v) {
+    public void onTest1(View v) {
         Intent it = new Intent();
         it.setClass(MainActivity.this, EmoActivity.class);
         startActivity(it);
     }
+
+    /**
+     * EditText禁止粘贴
+     * @param
+     * @return
+     */
+    public void onTest2(View v) {
+        startActivity(new Intent(MainActivity.this, TestActivity2.class));
+    }
+
+
+
+
 }
